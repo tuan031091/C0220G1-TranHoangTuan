7ad49a754331d8d274fd1e28f277e2a17cef39ff
Your Name <tuan031091.@gmail.com>
2020-02-28 14:51:40 +0700

    bai tap

--- 1.Module 1/CayStudyModule1/Module1_AccountName.js
+++ 1.Module 1/CayStudyModule1/Module1_AccountName.js
@@ -30,6 +30,31 @@ function payment() {
     Payment.value = Price.value * RentDays.value * (1 - Discount.value / 100)
 }
 
+function xacnhan() {
+    let FullName = document.getElementById('fullName').value;
+    let Gender = document.getElementById('gender').value;
+    let IdCard = document.getElementById('idCard').value;
+    let PhoneNumber = document.getElementById('phoneNumber').value;
+    let BirthDay = document.getElementById('birthDay').value;
+    let Email = document.getElementById('email').value;
+    let Address = document.getElementById('address').value;
+    let KindCustomer = document.getElementById('kindCustomer').value;
+    let AmountPeople = document.getElementById('amountPeople').value;
+    let RentDays = document.getElementById('rentDays').value;
+    let KindService = document.getElementById('kindService').value;
+    let Price = document.getElementById('price1').value;
+    let KindRoom = document.getElementById('kindRoom').value;
+    let Discount = document.getElementById('discount').value;
+    let payment = document.getElementById('payment').value;
+    alert('Ten ban la : ' + FullName + "\n" + 'Gioi tinh :' + Gender + '\n' +
+        'ID la :' + IdCard + '\n' + 'Phone :' + PhoneNumber + '\n' + 'Your Bithday :' + BirthDay + '\n' +
+        'Email :' + Email + '\n' + "Address : " + Address + '\n' + 'Kind of Customer' + KindCustomer + '\n' +
+        'Amount of people :' + AmountPeople + '\n' + 'Rent days :' + RentDays + '\n' + 'Kind of service :' +
+        KindService + '\n' + Price + 'Kind of Room : ' + KindRoom + '\n' + 'Discount : ' + Discount + '\n' +
+        'Tottal cost you must pay being : '
+        + '\n' + payment);
+}
+
 
 
 
